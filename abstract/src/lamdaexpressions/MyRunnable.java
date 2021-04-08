70dcc15f4c824b33c7042e25f47c843737f105e8
Aryan Akshay <varyanakshay.com>
2021-04-08 12:26:52 +0530

    Lamda Multi Threads

---
+++ abstract/src/lamdaexpressions/MyRunnable.java
@@ -0,0 +1,13 @@
+package lamdaexpressions;
+
+public class MyRunnable implements Runnable {
+	@Override
+	public void run() {
+		for(int i=0 ;i<5;i++)
+		{
+			System.out.println("in the Thread");
+		}
+
+	}
+}
+
---
+++ abstract/src/lamdaexpressions/ThreadTest.java
@@ -0,0 +1,19 @@
+package lamdaexpressions;
+
+import advance.thread.synchronization.TestInterruptingThread1;
+
+public class ThreadTest {
+	public static void main(String args[]) {
+		Runnable r =()->{
+			for(int i=0;i<5;i++) {
+				System.out.println("child thread");
+			}
+		};
+		Thread t = new Thread(r);
+		t.start();
+		for(int i=0;i<5;i++) {
+			System.out.println("main thread");
+		}
+	}
+
+}
