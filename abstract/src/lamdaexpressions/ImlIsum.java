5eefccad199d50cb5c6df827444c4df298f045e4
Aryan Akshay <varyanakshay.com>
2021-04-08 12:26:28 +0530

    Lamda life before interfaces

---
+++ abstract/src/lamdaexpressions/ImlIsum.java
@@ -0,0 +1,11 @@
+package lamdaexpressions;
+
+public class ImlIsum implements lSum  {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
---
+++ abstract/src/lamdaexpressions/lifebfuncinterfaces.java
@@ -0,0 +1,9 @@
+package lamdaexpressions;
+
+public class lifebfuncinterfaces {
+	public static void main(String[] args) {
+		ImlIsum isum = new ImlIsum();
+		System.out.println(isum.add(10,20));
+	}
+
+}
