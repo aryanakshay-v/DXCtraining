9783661664efde4aadcd12294c334d2175f33dbd
Aryan Akshay <varyanakshay.com>
2021-04-09 12:41:47 +0530

    Predicate with consumer

--- abstract/src/inbuilt/functionalInterface/functions/ConsumerTest.java
+++ abstract/src/inbuilt/functionalInterface/functions/ConsumerTest.java
@@ -2,16 +2,22 @@ package inbuilt.functionalInterface.functions;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class ConsumerTest {
 	static ArrayList<JavaStudent> javastudents = studenttest.populateStudents();
 	public static void main(String[] args) {
+		Predicate<JavaStudent> p = s-> s.marks >=60;
+
 		Consumer<JavaStudent> c = s -> System.out.println(s.name);
-	
+
+		System.out.println("Marks above 60 ");
 		for(JavaStudent student  : javastudents) {
-			c.accept(student);
+			if(p.test(student)) {
+				c.accept(student);
+			}
 		}
 	}
-	
+
 
 }
